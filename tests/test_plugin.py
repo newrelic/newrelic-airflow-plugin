c37df0bf7c6f11dd3004b6603430ddeb06fcffd8
Loren Arthur <larthur@newrelic.com>
2020-05-19 09:29:04 -0700

    Add tests for sending metrics.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -16,6 +16,8 @@ import pytest
 import datetime
 import os
 
+from newrelic_telemetry_sdk.client import MetricClient, HTTPResponse
+
 
 @pytest.fixture(scope="module")
 def stats():
@@ -26,6 +28,63 @@ def stats():
     assert pid not in Stats._batches
     yield Stats
     assert pid in Stats._batches
+
+
+@pytest.fixture
+def capture_sent(monkeypatch):
+    _metrics = []
+
+    def send_batch(instance, items, common):
+        _metrics.extend(items)
+        return HTTPResponse(status=200)
+
+    monkeypatch.setattr(MetricClient, "send_batch", send_batch)
+    yield _metrics
+
+
+def test_send_on_ti_success(stats, capture_sent):
+    # Record some metrics
+    stats.incr("test.incr")
+    stats.gauge("test.gauge", 12)
+    stats.timing("tests.timing", 1.32)
+    # No metrics should have been sent yet
+    assert len(capture_sent) == 0
+    # Record a ti_successes metric
+    stats.incr("ti_successes")
+    # All four metrics should be sent
+    assert len(capture_sent) == 4
+
+
+def test_on_dagrun_failure(stats, capture_sent):
+    # Record some metrics
+    stats.incr("test.incr")
+    stats.gauge("test.gauge", 12)
+    stats.timing("tests.timing", 1.32)
+    # No metrics should have been sent yet
+    assert len(capture_sent) == 0
+    # Record a dagrun.duration.failure metric
+    stats.timing("dagrun.duration.failure", 0.5)
+    # All four metrics should be sent
+    assert len(capture_sent) == 4
+
+
+def test_send_on_threshold(monkeypatch, stats, capture_sent):
+    from newrelic_airflow_plugin.newrelic_plugin import NewRelicStatsLogger
+
+    # monkeypatch to send after 5 metrics are recorded
+    monkeypatch.setattr(NewRelicStatsLogger, "SEND_THRESHOLD", 5)
+
+    stats.incr("test.incr.a")
+    stats.incr("test.incr.b")
+    stats.gauge("test.gauge.a", 12)
+    stats.gauge("test.gauge.b", 12)
+    stats.timing("test.timing", 1.32)
+    # No metrics should have been sent yet
+    assert len(capture_sent) == 0
+    # Record one more metric to pass the threshold
+    stats.timing("dagrun.duration.dag", 0.5)
+    # All six metrics should be sent
+    assert len(capture_sent) == 6
 
 
 def test_incr(stats):
